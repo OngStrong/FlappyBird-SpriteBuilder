aaa76df5d273c8de2c0ad39f62665c2cb33248be
Evie and Alex <EvieAndAlex@Evies-MacBook-Air.local>
2014-06-26 11:52:38 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -16,13 +16,7 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
-    timeSinceObstacle += delta;
     
-    if (timeSinceObstacle > 2.0f)
-    {
-        [self addObstacle];
-        
-        timeSinceObstacle = 0.0f;
 }
 
 // put new methods here
